a771e98ca882c887a1dadc6b5a8e94247c3c3869
gehlot2266 <75985096+gehlot2266@users.noreply.github.com>
2022-05-09 22:09:42 +0530

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021-2022 by @FunTwenty_4,.
+#
+# This file is part of < @FunTwenty_4 > project,
+# and is released under the "GNU v3.0 License Agreement".
+#
+# All rights reserved.
+#
+
+from .config import *
